55f6ca233f6ced659fdf000bb1f69c857ece0a24
Ameer Bajwa <ameerbajwa@gmail.com>
2021-05-10 11:06:24 -0700

    Created first controller

---
+++ MovieLookup/Controllers/MovieLookupViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MovieLookupViewController.swift
+//  MovieLookup
+//
+//  Created by Ameer Bajwa on 5/10/21.
+//  Copyright © 2021 Ameer Bajwa. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MovieLookupViewController {
+    
+}
