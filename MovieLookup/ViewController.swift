aec4ff712acbbe8d7f18c59dece1098886f7a2bf
Ameer Bajwa <ameerbajwa@gmail.com>
2021-05-10 10:57:47 -0700

    Created new project MovieLookup

---
+++ MovieLookup/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieLookup
+//
+//  Created by Ameer Bajwa on 5/10/21.
+//  Copyright © 2021 Ameer Bajwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
